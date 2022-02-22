294e096b356a876c5872074745365abb53797b42
Nóide Alves <83990543+noidealves@users.noreply.github.com>
2022-02-22 08:16:29 -0300

    Create alphanumeric_increment.php
    Usage: 
    
    $alphanum_str = '3sGWi';
    $alphanum_str = increment($alphanum_str); 
    
    Result: 3sGWj

---
+++ alphanumeric_increment.php
@@ -0,0 +1,22 @@
+<?php
+
+function increment($alphanum) {
+    $alphanum = preg_replace("/[^A-Za-z0-9]/", '', $alphanum) == "" ? "0" : $alphanum;
+    
+    for ($i = strlen($alphanum) - 1; $i >= 0; --$i) {
+        $c = $alphanum[$i];
+        $c = $c != "9" ? $c != "z" ? $c != "Z" ? ++$c : "0" : "A" : "a";
+        $alphanum[$i] = $c;
+        
+        if ($c == "0" && $i == 0) {
+            $alphanum .= "0";
+        }
+        else if ($c != "0") {
+            break;
+        }
+    }
+    
+    return $alphanum;
+}
+
+?>
